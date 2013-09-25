661af168e755327dd0bc67c7939e8f70dc860d26
Anthony Mace <amace93@gmail.com>
2013-09-24 22:31:43 -0700

    description of prog added

--- src/program4.java
+++ src/program4.java
@@ -1,5 +1,7 @@
 //Anthony Mace  CSC205AB
-//This program ...
+//This program is designed to set up two different classes
+//a DeckHand class, and a Card class. These two classes are then
+//used and tested in the program4 class
 
 
 public class program4 {
