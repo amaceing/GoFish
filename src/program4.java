14a3d312a6a7211b72edbb907221ed62cef87a21
Anthony Mace <amace93@gmail.com>
2013-09-25 23:34:09 -0700

    implemented count and popCard/testing

--- src/program4.java
+++ src/program4.java
@@ -10,16 +10,26 @@ public class program4 {
     public static void main(String[] args) {
         Card card1 = new Card(1, 2);
         Card card2 = new Card(12, 3);
-        Card card3 = new Card(6, 4);
+        Card card3 = new Card(1, 4);
         Card card4 = new Card(8, 1);
+        Card card5 = new Card(1, 2);
+        Card card6 = new Card(8, 4);
         DeckHand deck1 = new DeckHand();
         deck1.pushCard(card1);
         deck1.pushCard(card2);
+        deck1.pushCard(card5);
         System.out.println(deck1);
         DeckHand deck2 = new DeckHand();
         deck2.pushCard(card3);
         deck2.pushCard(card4);
+        deck2.pushCard(card6);
         System.out.println(deck2);
+        System.out.println(deck1.getSize());
+        System.out.println(deck2.getSize());
+        System.out.println(deck1.count(1));
+        System.out.println(deck2.count(8));
+        System.out.println(deck1.popCard(1));
+        System.out.println(deck2.popCard(8));
         System.out.println(deck1.getSize());
         System.out.println(deck2.getSize());
     }
@@ -61,6 +71,7 @@ class Card {
         return _suitValue;
     }
 
+    //Prints out a Card
     public String toString() {
         String face = "";
         String suit = "";
@@ -117,15 +128,20 @@ class DeckHand {
     private int cardCount;
     private Card[] deck;
 
+    //Constructs a DeckHand
     public DeckHand() {
         cardCount = 0;
         deck = new Card[CARDS];
     }
 
+    //Returns size of DeckHand
     public int getSize() {
         return cardCount;
     }
 
+    //Adds card to DeckHand and creates a larger
+    //array and copies contents from old array if
+    //expansion is needed
     public void pushCard(Card card) {
         if (cardCount == deck.length) {
             Card[] temp = new Card[deck.length * 2];
@@ -138,17 +154,32 @@ class DeckHand {
         cardCount++;
     }
 
+    //Returns the number of times a given value
+    //occurs in DeckHand
+    public int count(int faceValue) {
+        int count = 0;
+        for (int i = 0; i < cardCount; i++) {
+            if (faceValue == deck[i].getFaceValue()) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //Returns one instance of a Card with given value
+    //From DeckHand
     public Card popCard(int faceValue) {
         Card found = new Card(1, 1);
-        for (int i = 0; i < deck.length; i++) {
+        for (int i = 0; i < cardCount; i++) {
             if (faceValue == deck[i].getFaceValue()) {
-                System.out.println(deck[i]);
                 found = deck[i];
             }
         }
+        cardCount--;
         return found;
     }
 
+    //Prints out entire deck
     public String toString() {
         String currentDeck = "";
         for (int i = 0; i < deck.length; i++) {
