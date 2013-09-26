b3548f611809f52f7caf4d079473ae86b4b563eb
Anthony Mace <amace93@gmail.com>
2013-09-25 19:57:12 -0700

    changed static strings to static array of strings in fave/suit values

--- src/program4.java
+++ src/program4.java
@@ -24,25 +24,13 @@ class Card {
 
     //Named constants
     //Face values
-    private static final String ACE_VALUE = "Ace";
-    private static final String TWO_VALUE = "Two";
-    private static final String THREE_VALUE = "Three";
-    private static final String FOUR_VALUE = "Four";
-    private static final String FIVE_VALUE = "Five";
-    private static final String SIX_VALUE = "Six";
-    private static final String SEVEN_VALUE = "Seven";
-    private static final String EIGHT_VALUE = "Eight";
-    private static final String NINE_VALUE = "Nine";
-    private static final String TEN_VALUE = "Ten";
-    private static final String JACK_VALUE = "Jack";
-    private static final String QUEEN_VALUE = "Queen";
-    private static final String KING_VALUE = "King";
+    private static final String[] FACE_VALUES = {"Ace", "Two", "Three", "Four",
+                                         "Five", "Six", "Seven", "Eight", "Nine",
+                                         "Ten", "Jack", "Queen", "King"};
 
     //Suit values
-    private static final String HEARTS_VALUE = " of Hearts";
-    private static final String DIAMONDS_VALUE = " of Diamonds";
-    private static final String CLUBS_VALUE = " of Clubs";
-    private static final String SPADES_VALUE = " of Spades";
+    private static final String[] SUIT_VALUES = {" of Hearts", " of Diamonds",
+                                                 " of Clubs", " of Spades"};
 
     //Constructs a card with values passed
     public Card(int faceValue, int suitValue) {
@@ -71,42 +59,42 @@ class Card {
 
         //Face values
         if (_faceValue == 1) {
-            face = ACE_VALUE;
+            face = FACE_VALUES[0];
         } else if (_faceValue == 2) {
-            face = TWO_VALUE;
+            face = FACE_VALUES[1];
         } else if (_faceValue == 3) {
-            face = THREE_VALUE;
+            face = FACE_VALUES[2];
         } else if (_faceValue == 4) {
-            face = FOUR_VALUE;
+            face = FACE_VALUES[3];
         } else if (_faceValue == 5) {
-            face = FIVE_VALUE;
+            face = FACE_VALUES[4];
         } else if (_faceValue == 6) {
-            face = SIX_VALUE;
+            face = FACE_VALUES[5];
         } else if (_faceValue == 7) {
-            face = SEVEN_VALUE;
+            face = FACE_VALUES[6];
         } else if (_faceValue == 8) {
-            face = EIGHT_VALUE;
+            face = FACE_VALUES[7];
         } else if (_faceValue == 9) {
-            face = NINE_VALUE;
+            face = FACE_VALUES[8];
         } else if (_faceValue == 10) {
-            face = TEN_VALUE;
+            face = FACE_VALUES[9];
         } else if (_faceValue == 11) {
-            face = JACK_VALUE;
+            face = FACE_VALUES[10];
         } else if (_faceValue == 12) {
-            face = QUEEN_VALUE;
+            face = FACE_VALUES[11];
         } else if (_faceValue == 13) {
-            face = KING_VALUE;
+            face = FACE_VALUES[12];
         }
 
         //Suit values
         if (_suitValue == 1) {
-            suit = HEARTS_VALUE;
+            suit = SUIT_VALUES[0];
         } else if (_suitValue == 2) {
-            suit = DIAMONDS_VALUE;
+            suit = SUIT_VALUES[1];
         } else if (_suitValue == 3) {
-            suit = CLUBS_VALUE;
+            suit = SUIT_VALUES[2];
         } else if (_suitValue == 4) {
-            suit = SPADES_VALUE;
+            suit = SUIT_VALUES[3];
         }
 
         return face + suit;
